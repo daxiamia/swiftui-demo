c4f96609b8c5002fe1ea2ee5cd1e787b3ad9e404
weichu <weichu@mgtv.com>
2024-03-28 15:45:24 +0800

    Initial Commit

---
+++ ImageGlary/ImageGlaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGlaryApp.swift
+//  ImageGlary
+//
+//  Created by daxia on 2024/3/28.
+//
+
+import SwiftUI
+
+@main
+struct ImageGlaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
